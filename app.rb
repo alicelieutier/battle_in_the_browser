154e0857117b0a2d26e94a6d96d616ed0821b63a
Alice Lieutier <alice@lieutier.me>
2018-07-20 12:19:14 +0100

    refactor to POST/redirect/GET

--- app.rb
+++ app.rb
@@ -1,13 +1,21 @@
 require "sinatra/base"
 
 class Battle < Sinatra::Base
+  enable :sessions
+
   get '/' do
     erb :index
   end
 
   post '/names' do
-    @player_1 = params[:player_1]
-    @player_2 = params[:player_2]
+    session[:player_1] = params[:player_1]
+    session[:player_2] = params[:player_2]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player_1 = session[:player_1]
+    @player_2 = session[:player_2]
     erb :play
   end
 end
